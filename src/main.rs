238e9b25827d3952af6eed44dc912004b3519fb4
Casey Fryer <caseyfryer@gmail.com>
2023-05-09 15:48:20 -0700

    chapter 3

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::net::TcpListener;
+
+use::zero2prod::run;
+
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    run(listener)?.await
+}
