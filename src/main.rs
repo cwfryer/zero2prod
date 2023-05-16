1218d122ae9749ea7a79f836fd2156a009597d6f
Casey Fryer <caseyfryer@gmail.com>
2023-05-16 10:37:33 -0700

    chapter 4 telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-use::zero2prod::startup::run;
+use ::zero2prod::configuration::get_configuration;
+use ::zero2prod::startup::run;
+use ::zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
-    run(listener)?.await
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    // Get port from config
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind address");
+    run(listener, connection_pool)?.await
 }
